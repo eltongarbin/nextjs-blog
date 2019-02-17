f4ec0726b450966f5d424033ccd98f4897c01cf4
Elton Garbin <eltongarbin@gmail.com>
2019-02-16 23:20:16 -0200

    populate a nextjs application with data using async await

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Button, CardActions, CardHeader, Card } from '@material-ui/core';
+import 'isomorphic-fetch';
+
 import Header from '../components/Header';
 
-const Index = ({ title = 'Hello from next.js' }) => (
+const Index = ({ posts = [] }) => (
   <div>
     <Header />
-    <h2>{title}</h2>
+    {posts.map((p) => (
+      <Card key={p.id}>
+        <CardHeader title={p.title} />
+        <CardActions>
+          <Button variant="contained" color="primary" fullWidth>
+            Click to view posts!
+          </Button>
+        </CardActions>
+      </Card>
+    ))}
   </div>
 );
+
+Index.getInitialProps = async () => {
+  const response = await fetch(
+    `${process.env.BLOGGER_URL}?key=${process.env.API_KEY}`
+  );
+  const data = await response.json();
+
+  return { posts: data.items };
+};
 
 export default Index;
